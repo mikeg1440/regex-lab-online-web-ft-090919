941a484d77ae84b50af43c5e7cbfde36049d905a
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-12 02:29:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -22,7 +22,7 @@ def words_five_letters_long(text)
   resp = []
   
   txt_array.each do |word|
-    # binding.pry
+    binding.pry
     if word.scan(/[a-zA-Z]{5}/).to_s == word
       resp << word
       binding.pry
